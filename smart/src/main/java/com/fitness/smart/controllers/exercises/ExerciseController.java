6f0563ed7f6e99e21f197b79d720bc3a749a8450
Cristofer <cristofermil@gmail.com>
2025-01-11 02:21:10 +0100

    endpoint

---
+++ smart/src/main/java/com/fitness/smart/controllers/exercises/ExerciseController.java
@@ -0,0 +1,12 @@
+package com.fitness.smart.controllers.exercises;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.fitness.smart.variables.EndPoint.BASE_URL;
+
+@RequestMapping(BASE_URL)
+@RestController
+public class ExerciseController {
+    
+}
---
+++ smart/src/main/java/com/fitness/smart/variables/EndPoint.java
@@ -0,0 +1,5 @@
+package com.fitness.smart.variables;
+
+public class EndPoint {
+    public static final String BASE_URL="/fitness/api";
+}
